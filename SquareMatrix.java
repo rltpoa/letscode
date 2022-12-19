610ea16fb156271fe48c2d04609e5f1e56b71231
rlt <rltpoa@users.noreply.github.com>
2022-12-19 10:41:36 -0300

    padronização em inglês para SquareMatrix.java

--- SquareMatrix.java
+++ SquareMatrix.java
@@ -1,23 +1,23 @@
 public class SquareMatrix {
 
     public static void main(String[] args) {
-        int[][] matriz = new int[3][3];
-        matriz[0][0] = 1;
-        matriz[0][1] = 2;
-        matriz[0][2] = 3;
-        matriz[1][0] = 4;
-        matriz[1][1] = 5;
-        matriz[1][2] = 6;
-        matriz[2][0] = 7;
-        matriz[2][1] = 8;
-        matriz[2][2] = 9;
-        System.out.println(isSquareMatriz(matriz));
+        int[][] matrix = new int[3][3];
+        matrix[0][0] = 1;
+        matrix[0][1] = 2;
+        matrix[0][2] = 3;
+        matrix[1][0] = 4;
+        matrix[1][1] = 5;
+        matrix[1][2] = 6;
+        matrix[2][0] = 7;
+        matrix[2][1] = 8;
+        matrix[2][2] = 9;
+        System.out.println(isSquareMatrix(matrix));
     }
 
-    public static boolean isSquareMatriz(int[][] matriz) {
-        int linhas = matriz.length;
-        int colunas = matriz[0].length;
-        if (linhas == colunas) {
+    public static boolean isSquareMatrix(int[][] matrix) {
+        int lines = matrix.length;
+        int columns = matrix[0].length;
+        if (lines == columns) {
             return true;
         } else {
             return false;
